ac6fc2c566fac26f52f2e67013920a29bdc2d8e2
Stark <narutoulrimate@gmail.com>
2020-05-12 18:59:03 +0300

    apu is real

--- app/src/main/java/com/example/filmforfriends/MainActivity.kt
+++ app/src/main/java/com/example/filmforfriends/MainActivity.kt
@@ -4,10 +4,14 @@
 
 package com.example.filmforfriends
 
-import android.app.ProgressDialog.show
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import androidx.core.view.GravityCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.image_item.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,8 +19,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val img = BitmapFactory.decodeResource(resources, R.layout.image_item)
+        val round = RoundedBitmapDrawableFactory.create(resources, img)
+
+        round.cornerRadius = 25f
+        Image_item.setImageDrawable(round)
+
         supportFragmentManager.beginTransaction()
-            .replace(R.id.framelayout, DrammaFragment())
+            .replace(R.id.Main_Activiti, DrammaFragment())
             .commit()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp)
         }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home){
+            if(drawlerLayout.isDrawerOpen(GravityCompat.START)){
+                drawlerLayout.closeDrawer(GravityCompat.START)
+            }else{
+                drawlerLayout.openDrawer(GravityCompat.START)
+            }
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     }
